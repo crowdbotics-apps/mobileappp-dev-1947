df7106963fbd45da877f855e3ac7851a084a7e40
Crowdbotics <team@crowdbotics.com>
2020-03-12 07:58:21 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,6 +7,28 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
+    new = models.BigIntegerField(null=True, blank=True,)
+    neww = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_neww",
+    )
+    newww = models.ForeignKey(
+        "home.HomePage",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_newww",
+    )
+    newwww = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_newwww",
+    )
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
