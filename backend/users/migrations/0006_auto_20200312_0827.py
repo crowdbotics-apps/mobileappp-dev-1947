6af70572423c5772a455cc54f90840e2d055f8f8
Crowdbotics <team@crowdbotics.com>
2020-03-12 08:34:25 +0000

    Update models

--- backend/users/migrations/0006_auto_20200312_0827.py
+++ backend/users/migrations/0006_auto_20200312_0827.py
@@ -6,16 +6,10 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0005_user_neww'),
+        ("users", "0005_user_neww"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='neww',
-        ),
-        migrations.RemoveField(
-            model_name='user',
-            name='newwww',
-        ),
+        migrations.RemoveField(model_name="user", name="neww",),
+        migrations.RemoveField(model_name="user", name="newwww",),
     ]
